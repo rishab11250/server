0061a4485e6f97864da1b07f420b7e85957445ca
rishab11250 <rishab.chandgothia.cg@gmail.com>
2026-02-04 11:54:46 +0530

    server made

--- index.js
+++ index.js
@@ -1,10 +1,41 @@
-const express = require('express');
+const express = require("express");
+
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Express server is running');
+const users = [
+    { id: 1, name: "Arjun", role: "student" },
+    { id: 2, name: "Priyesha", role: "mentor" }
+];
+
+
+app.get("/", (req, res) => {
+    res.send("Express server is running");
+});
+
+app.get("/users", (req, res) => {
+    console.log(users)
+    res.status(200).json(users);
+});
+
+
+app.get("/user/test/:user_id", (req, res) => {
+    console.log(req);
+    res.status(200).json(users);    // send response
+});
+
+
+
+app.get("/users/:id", (req, res) => {
+    const userId = Number(req.params.id);
+    const user = users.find(u => u.id === userId);
+
+    if (!user) {
+        return res.status(404).json({ message: "User not found" });
+    }
+
+    res.status(200).json(user);
 });
 
 app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+    console.log("Server started on port 3000");
 });
